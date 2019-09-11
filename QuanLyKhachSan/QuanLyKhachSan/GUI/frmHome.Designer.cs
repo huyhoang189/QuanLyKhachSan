c360c2d2281a163308fb4771f8c77571ec69b0b5
huyhoang189 <vohuyhoang189@gmail.com>
2019-09-11 15:23:31 +0700

    add form frmBookRoom and connect action click book room from frmHome

--- QuanLyKhachSan/QuanLyKhachSan/GUI/frmHome.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/GUI/frmHome.Designer.cs
@@ -331,6 +331,7 @@
             this.btn_bookRoom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btn_bookRoom.Textcolor = System.Drawing.Color.White;
             this.btn_bookRoom.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bookRoom.Click += new System.EventHandler(this.btn_bookRoom_Click);
             // 
             // bunifuFlatButton4
             // 
