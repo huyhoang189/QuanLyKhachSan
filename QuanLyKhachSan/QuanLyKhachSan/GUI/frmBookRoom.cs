78b515f452f537fc4f2a5f8b34d6476942ccb8be
khanhtoan99gvc <khanhtoan99gvc@gmail.com>
2019-10-25 23:18:05 +0700

    thong tin khach hang

--- QuanLyKhachSan/QuanLyKhachSan/GUI/frmBookRoom.cs
+++ QuanLyKhachSan/QuanLyKhachSan/GUI/frmBookRoom.cs
@@ -91,8 +91,44 @@ namespace QuanLyKhachSan.GUI
             txt_MaDatPhong.Text = check_MaDatPhong();
             load();
         }
-
+        bool Check_InfoCustomer()
+        {
+            if(txt_CMND.Text ==""|| txt_DiaChi.Text==""||txt_HoTen.Text==""||txt_QuocTich.Text=="")
+            {
+                return false;
+            }
+            return true;
+        }
         private void bunifuFlatButton3_Click(object sender, EventArgs e)
+        {
+           
+            if (Check_InfoCustomer() == false)
+            {
+                MessageBox.Show("Thông tin khách hàng chưa đầy đủ!");
+            }
+            else
+            {
+                if(check_Customer(txt_CMND.Text.Trim())==false)
+                {
+                    query = "USP_InsertCustomer";
+                    using (conn = new SqlConnection(cnn.getConnectionString(1)))
+                    {
+                        cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@customerName", txt_HoTen.Text);
+                        cmd.Parameters.AddWithValue("@idCustomerType", 1);
+                        cmd.Parameters.AddWithValue("@idCard", txt_CMND.Text);
+                        cmd.Parameters.AddWithValue("@address", txt_DiaChi.Text);
+                        cmd.Parameters.AddWithValue("@dateOfBirth", dtp_NgaySinh.Value);
+                        cmd.Parameters.AddWithValue("@phoneNumber", Convert.ToInt32(txt_SDT.Text));
+
+
+                    }
+                        
+                }
+            }
+        }
+
+        private void gunaLabel12_Click(object sender, EventArgs e)
         {
 
         }
